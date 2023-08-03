5668841abe602c9e85bf51702f7a7b130fbde080
Izam Ghali <izamghali18@gmail.com>
2023-08-03 14:09:10 +0700

    created BodyThought component, addThought function so we can add & display thought

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './components';
-import TabTitle from './components';
-
-let test = <p>Still under maintenance</p>;
+import { TabTitle, BodyThought } from './components';
 
 function App() {
+
+  const [ thoughts, setThoughts ] = useState([{
+    thoughtId: 1,
+    thoughtText: "hello world"
+  }, {
+    thoughtId: 2, 
+    thoughtText: "This will be gone in 15 seconds"
+  }])
+
+  const addThought = (thought) => {
+    setThoughts(prevThoughts => 
+      [...prevThoughts, thought]
+    );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <TabTitle />
         <img src={logo} className="App-logo" alt="logo" />
-        { test }
+        <BodyThought addThought={addThought}/>
+
+        <ul>
+          { thoughts.map(thought => 
+            <li>{thought.thoughtText}</li>
+          ) }
+        </ul>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,6 +40,7 @@ function App() {
         >
           Learn React
         </a>
+        
       </header>
     </div>
   );
--- src/components.js
+++ src/components.js
@@ -1,25 +1,12 @@
 import React, { useState, useEffect } from 'react';
 
+export function TabTitle() {
 
-function Navbar() {
-
-    return (
-        <nav>
-            <ul>
-                <li>Home</li>
-                <li>Projects</li>
-            </ul>
-        </nav>
-    );
-}
-
-export default function TabTitle() {
-
-    const [ title, setTitle ] = useState('React App');
+    const [ title, setTitle ] = useState('');
 
     useEffect(() => {
         document.title = title;
-    })
+    }, [title])
 
     const handleChange = ({target}) => {
         const typedInput = target.value;
@@ -29,7 +16,46 @@ export default function TabTitle() {
     return (
         <>
             <p>Type to change tab title</p>
-            <input onChange={handleChange} value={title} type="text" />
+            <input onChange={handleChange} placeholder="Type to change tab title" value={title} type="text" />
+        </>
+    )
+}
+
+export function BodyThought(props) {
+
+    const [ text, setText ] = useState('') 
+    // if we don't put empty string, it'll throw TypeError
+    // because we're gonna use variable text.length to check whether it's 0 or not
+    // and it's not gonna work it's undefined
+
+    const handleTextChange = ({ target }) => {
+        setText(target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        // event.preventDefault() prevents default setting where it loads the page, so we prevent it
+        
+        if (text.length > 0) {
+            const thought = { // creating a new thought object
+                thoughtId : 3,
+                thoughtText: text,
+            }
+            props.addThought(thought); // the new object is passed through props.addthought
+            setText('');
+        } 
+    }
+
+    return (
+        <>
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text" 
+                onChange={handleTextChange} // if we don't fill onChange, we can't type
+                value={text} 
+                placeholder='Type your thought'/>
+            <input type="submit" value="Add thought"/>
+        </form>
         </>
     )
 }
